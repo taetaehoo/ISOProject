e1f6c51eeb876e96ab0eeaf8e72ff1a09f18da21
taetaehoo <hapkldo09@naver.com>
2023-10-08 20:24:16 +0900

    2023-10-08 20:23
    bluetooth serial 통신 & 데이터 수신

--- ISOProject/Program.cs
+++ ISOProject/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿using System;
+using System.IO.Ports;
+
+class Program
+{
+    static void Main()
+    {
+        string portName = "COM4";
+        int baudRate = 9600;
+
+        SerialPort serialPort = new SerialPort(portName, baudRate);
+
+        try
+        {
+            serialPort.Open();
+            serialPort.DataReceived += SerialPort_DataReceived;
+
+            Console.WriteLine("Bluetooth 시리얼 통신이 연결되었습니다. 데이터를 수신합니다.");
+
+            Console.ReadLine();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"오류: {ex.Message}");
+        }
+        finally
+        {
+            if (serialPort.IsOpen)
+                serialPort.Close();
+        }
+    }
+
+    private static void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+    {
+        SerialPort serialPort = (SerialPort)sender;
+        string receivedData = serialPort.ReadExisting();
+        Console.WriteLine($"수신된 데이터: {receivedData}");
+    }
+}
